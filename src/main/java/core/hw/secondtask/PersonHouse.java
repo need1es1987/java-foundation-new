a0b0dd98ebeb5c5e2c83aa2ea00acb31e60effd9
Андрей <asmihailov1987@gmail.com>
2020-07-16 00:51:56 +0300

    Add third task of home work OOP2

--- src/main/java/core/hw/secondtask/PersonHouse.java
+++ src/main/java/core/hw/secondtask/PersonHouse.java
@@ -3,9 +3,20 @@ package core.hw.secondtask;
 public class PersonHouse {
 
     public static void main(String[] args) {
-        Person man = new Man("Andrew",35);
+        Person man = new Man("Andrew",25);
         man.printAge();
-        Person woman = new Woman("Maria",28);
+        Person woman = new Woman("Maria",18);
         woman.printAge();
+
+        Person[] person = new Person[2];
+        person[0] = man;
+        person[1] = woman;
+
+        for (int i = 0; i < person.length ;i++) {
+            System.out.println("Возраст " + person[i].getName() + " = " + person[i].getAge());
+
+        }
     }
+
+
 }
