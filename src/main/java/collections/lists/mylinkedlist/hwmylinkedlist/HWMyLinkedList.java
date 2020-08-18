fd1bbe4e80b93c5dad3bb4fddf4d18fe8563787b
Андрей <asmihailov1987@gmail.com>
2020-08-18 21:29:11 +0300

    Add home work MyLinkedList, methods and tests.

---
+++ src/main/java/collections/lists/mylinkedlist/hwmylinkedlist/HWMyLinkedList.java
@@ -0,0 +1,244 @@
+package collections.lists.mylinkedlist.hwmylinkedlist;
+
+import collections.lists.mylinkedlist.Node;
+import jdk.nashorn.api.scripting.ScriptUtils;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.swing.plaf.IconUIResource;
+import javax.xml.validation.SchemaFactoryConfigurationError;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class HWMyLinkedList {
+    private NodeHW head;
+
+
+    public int size() {
+        if (head == null) {
+            return 0;
+        }
+        int size = 0;
+        NodeHW curNode = head;
+        while (curNode != null) {
+            size++;
+            curNode = curNode.getNext();
+        }
+        return size;
+    }
+
+    public boolean isEmpty() {
+        if (head == null) return true;
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        if (head == null) {
+            return false;
+        }
+
+        NodeHW curNode = head;
+        if (curNode.getValue().equals(o) == true) {
+            return true;
+        }
+
+        while (curNode.getNext() != null) {
+            if (curNode.getNext().getValue().equals(o) == true) {
+                return true;
+            }
+            curNode = curNode.getNext();
+        }
+        return false;
+    }
+
+
+    public boolean add(Object o) {
+        if (head == null) {
+            head = new NodeHW(o, null);
+        } else {
+            NodeHW curNode = head;
+            while (curNode.getNext() != null) {
+                curNode = curNode.getNext();
+            }
+            curNode.setNext(new NodeHW(o, null));
+        }
+        return true;
+    }
+
+    public boolean remove(Object o) {
+        if (head == null) {
+            return false;
+        } else {
+            NodeHW removeElement = head;
+            if (removeElement.getValue().equals(o) == true) {
+                head = head.getNext();
+            } else {
+                NodeHW curRemoveNode = head;
+                while (curRemoveNode.getNext() != null) {
+                    if (curRemoveNode.getNext().getValue().equals(o) == true) {
+                        break;
+                    }
+                    curRemoveNode = curRemoveNode.getNext();
+                }
+                removeElement = curRemoveNode.getNext();
+                curRemoveNode.setNext(removeElement.getNext());
+                removeElement.setNext(null);
+            }
+        }
+        return true;
+    }
+
+    public void clear() {
+        if (head != null) {
+            head = null;
+        }
+    }
+
+    public Object get(int index) {
+        if (isCorrectIndex(index)) {
+            NodeHW curNode = head;
+            int curIndex = 0;
+
+            while (curIndex != index) {
+                curIndex++;
+                curNode = curNode.getNext();
+            }
+            return curNode.getValue();
+        }
+        return null;
+    }
+
+    public Object set(int index, Object element) {
+        if (isCorrectIndex(index)) {
+            NodeHW curNode = head;
+            int curIndex = 0;
+
+            while (curIndex != index) {
+                curIndex++;
+                curNode = curNode.getNext();
+            }
+
+            Object setElem = curNode.getValue();
+            curNode.setValue(element);
+            return setElem;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+
+    public void add(int index, Object element) {
+        if (isCorrectIndex(index)) {
+            NodeHW curNode = head;
+            NodeHW prevNode = head;
+            int curIndex = 0;
+            if (index == 0) {
+                head = new NodeHW(element, curNode);
+            } else {
+                while (curIndex != index) {
+                    curIndex++;
+                    prevNode = curNode;
+                    curNode = curNode.getNext();
+                }
+                prevNode.setNext(new NodeHW(element, curNode.getNext()));
+            }
+
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public Object remove(int index) {
+        if (isCorrectIndex(index)) {
+            NodeHW removableNode;
+            if (index == 0) {
+                removableNode = head;
+                head = head.getNext();
+            } else {
+                NodeHW cureNode = head;
+                int curIndex = 0;
+
+                while (curIndex != index - 1) {
+                    curIndex++;
+                    cureNode = cureNode.getNext();
+                }
+                removableNode = cureNode.getNext();
+                cureNode.setNext(removableNode.getNext());
+                removableNode.setNext(null);
+            }
+            return removableNode;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+
+    private boolean isCorrectIndex(int index) {
+        if ((index >= 0) && (index < size())) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public int indexOf(Object o) {
+        if (head != null) {
+            NodeHW curNode = head;
+            int curIndex = 0;
+            while (curNode != null) {
+                if (curNode.getValue().equals(o) == true) {
+                    return curIndex;
+                }
+                curNode = curNode.getNext();
+                curIndex++;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object o) {
+        if (head != null) {
+            NodeHW curNode = head;
+            int curIndex = 0;
+            int index = 0;
+            int sumOfElements = 0;
+            while (curNode != null) {
+                if (curNode.getValue().equals(o) == true) {
+                    ++sumOfElements;
+                    break;
+                }
+                curNode = curNode.getNext();
+            }
+            if (sumOfElements == 0) {
+                return -1;
+            }
+            NodeHW curNextNode = head;
+            while (curNextNode != null) {
+                if (curNextNode.getValue().equals(o) == true) {
+                    index = curIndex;
+                }
+                curNextNode = curNextNode.getNext();
+                curIndex++;
+            }
+            return index;
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("HWMyLinkedList{ ");
+
+        if (head != null) {
+            NodeHW curNode = head;
+
+            while (curNode.getNext() != null) {
+                stringBuilder.append(curNode.getValue()).append(',');
+                curNode = curNode.getNext();
+            }
+            stringBuilder.append(curNode.getValue());
+        }
+        stringBuilder.append(" }");
+
+        return stringBuilder.toString();
+    }
+}
